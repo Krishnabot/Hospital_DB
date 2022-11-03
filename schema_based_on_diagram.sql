90fb25b3071c97024da16fc2493b4da5375f171d
Krishna <libraryismyparadise@gmail.com>
2022-11-03 16:43:30 +0545

    Create patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,2 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE hospital;
+
+CREATE TABLE patients(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(250),
+    date_of_birth DATE
+);
