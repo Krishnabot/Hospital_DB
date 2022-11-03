1103c9af951f20600282815867f9a33e92e0581d
Krishna <libraryismyparadise@gmail.com>
2022-11-03 16:57:56 +0545

    Add medical_histories Table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,10 @@ CREATE TABLE treatments (
     name VARCHAR(250),
     type VARCHAR(25)
 );
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(10)
+);
